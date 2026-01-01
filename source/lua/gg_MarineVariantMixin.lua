7bffb67a36c6ab20a422df489550103ec2474ce1
Ondrej Hruska <11602729+ZycaR@users.noreply.github.com>
2026-01-01 20:09:38 +0100

    fixed bigmac skin variants

--- source/lua/gg_MarineVariantMixin.lua
+++ source/lua/gg_MarineVariantMixin.lua
@@ -3,16 +3,21 @@
 	ZycaR (c) 2025
 ]]
 
-function MarineVariantMixin:GetMarineType()
-    return ConditionalValue(self.marineType == kMarineVariantsBaseType.female, kMarineVariantsBaseType.female, kMarineVariantsBaseType.male)
-end
-
-function MarineVariantMixin:GetMarineTypeString()
-    return GetMarineTypeLabel( self:GetMarineType() )
-end
-
+-- Override to always use default marine variant
 function MarineVariantMixin:GetVariantModel()
-    return MarineVariantMixin.kModelNames[ self:GetMarineTypeString() ][ kDefaultMarineVariant ]
+    local type = ConditionalValue(self.marineType == kMarineVariantsBaseType.female, kMarineVariantsBaseType.female, kMarineVariantsBaseType.male)
+    return MarineVariantMixin.kModelNames[ GetMarineTypeLabel( type ) ][ kDefaultMarineVariant ]
 end
 
-
+if Client then
+    local ns2_OnMarineSkinChanged = MarineVariantMixin.OnMarineSkinChanged
+    function MarineVariantMixin:OnMarineSkinChanged()
+        if self.clientVariant == self.variant and not self.forceSkinsUpdate then
+            return
+        end
+        if table.icontains( kRoboticMarineVariantIds, self.variant ) then
+            return -- Prevent changing to robotic variants
+        end
+        ns2_OnMarineSkinChanged(self)
+    end
+end
